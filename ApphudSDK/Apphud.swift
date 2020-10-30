a4bccdc2391eb634f81168034ff1c569dae8711e
Renat Kurbanov <ren6@apphud.com>
2020-10-30 11:48:53 +0300

    fix potential bug, where eligibility checking may not call callback at all in some cases (#35)

--- ApphudSDK/Apphud.swift
+++ ApphudSDK/Apphud.swift
@@ -10,7 +10,7 @@ import UIKit
 import StoreKit
 import UserNotifications
 
-internal let apphud_sdk_version = "0.18.5"
+internal let apphud_sdk_version = "0.18.6"
 
 public typealias ApphudEligibilityCallback = (([String: Bool]) -> Void)
 public typealias ApphudBoolCallback = ((Bool) -> Void)
