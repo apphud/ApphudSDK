fbcb42f244b4f1e20c489c8057dbae0d1b94d8df
Soslan-Bek <soslanbek.app@gmail.com>
2021-10-20 15:08:10 +0300

    Updated the structure and main controller for code readability

--- Example/ApphudSDKDemo/Screens/ViewController.swift
+++ Example/ApphudSDKDemo/Screens/ViewController.swift
@@ -12,10 +12,12 @@ import ApphudSDK
 
 typealias Callback = (() -> Void)
 
-class ViewController: UITableViewController{
+final class ViewController: UITableViewController {
+    
     var paywalls = [ApphudPaywall]()
     var rowsActions = [(String, Callback)]()
         
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -36,6 +38,45 @@ class ViewController: UITableViewController{
         
         reload()
     }
+}
+
+// MARK: - Apphud Delegate
+extension ViewController: ApphudDelegate {
+    func apphudDidFetchStoreKitProducts(_ products: [SKProduct]) {
+        self.reload()
+    }
+    
+    func apphudSubscriptionsUpdated(_ subscriptions: [ApphudSubscription]) {
+        self.reload()
+    }
+}
+
+// MARK: - Apphud UI Delegate
+extension ViewController: ApphudUIDelegate {
+    func apphudScreenPresentationStyle(controller: UIViewController) -> UIModalPresentationStyle {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            return .pageSheet
+        } else {
+            return .overFullScreen
+        }
+    }
+}
+
+// MARK: - Private Methods
+private extension ViewController {
+    @objc func reloadUI(){
+        reload()
+    }
+    
+    @objc func reload() {
+        tableView.reloadData()
+    }
+    
+    @objc func restore() {
+        Apphud.restorePurchases { _, _, _ in
+            self.reload()
+        }
+    }
     
     func setupRowActions() {
         rowsActions = [
@@ -53,10 +94,6 @@ class ViewController: UITableViewController{
             print("Not supported")
         }
     }
-    
-    @objc func reloadUI(){
-        reload()
-    }
             
     @objc func fetchRawReceipt() {
         Apphud.fetchRawReceiptInfo { receipt in
@@ -66,96 +103,6 @@ class ViewController: UITableViewController{
                 print("could not fetch raw receipt")
             }
         }
-    }
-    
-    @objc func restore(){
-        Apphud.restorePurchases { _, _, _ in
-            self.reload()
-        }
-    }
-    
-    @objc func reload(){
-        tableView.reloadData()
-    }
-    
-    //MARK:- TableView Delegate methods
-    
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        section == 0 ? "ACTIONS" : "PRODUCTS"
-    }
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        1 + paywalls.count
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        section == 0 ? rowsActions.count : paywalls[section - 1].products.count
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "productCell", for: indexPath)
-        
-        if indexPath.section == 0 {
-            cell.textLabel?.text = rowsActions[indexPath.row].0
-            cell.detailTextLabel?.text = nil
-        } else {
-            let paywall = paywalls[indexPath.section - 1]
-            let product = paywall.products[indexPath.item]
-            
-            let text = product.skProduct?.getFullSubscriptionInfoString() ?? "PRODUCT UNAVAILABLE: \(product.productId)"
-            cell.textLabel?.text = text
-            if let subscription = Apphud.subscriptions()?.first(where: {$0.productId == product.productId}) {
-                cell.detailTextLabel?.text = subscription.expiresDate.description(with: Locale.current) + "\nState: \(subscription.status.toStringDuplicate())\nIntroductory used:\(subscription.isIntroductoryActivated)".uppercased()
-            } else {
-                cell.detailTextLabel?.text = "Not active"
-            }
-        }
-    
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        if indexPath.section == 0 {
-            rowsActions[indexPath.row].1()
-            return
-        }
-        
-        let paywall = paywalls[indexPath.section - 1]
-        let product = paywall.products[indexPath.item]
-
-        if #available(iOS 12.2, *) {
-            if product.skProduct?.discounts.count ?? 0 > 0 {
-                // purchase promo offer
-                showPromoOffersAlert(product: product)
-            } else {
-                purchaseProduct(product: product)
-            }
-        } else {
-            purchaseProduct(product: product)
-        }
-       
-    }
-    
-    @available(iOS 12.2, *)
-    func showPromoOffersAlert(product : ApphudProduct) {
-        
-        let controller = UIAlertController(title: "You already have subscription for this product", message: "would you like to activate promo offer?", preferredStyle: .alert)
-        
-        for discount in product.skProduct?.discounts ?? [] {
-            controller.addAction(UIAlertAction(title: "Purchase Promo: \(discount.identifier!)", style: .default, handler: { act in
-                self.purchasePromo(product: product, promoID: discount.identifier!)
-            }))
-        }
-        
-        controller.addAction(UIAlertAction(title: "Purchase Product As Usual", style: .destructive, handler: { act in
-            self.purchaseProduct(product: product)
-        }))
-        
-        controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        present(controller, animated: true, completion: nil)
     }
     
     @available(iOS 12.2, *)
@@ -182,26 +129,86 @@ class ViewController: UITableViewController{
         controller.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         present(controller, animated: true, completion: nil)
     }
+    
+    @available(iOS 12.2, *)
+    func showPromoOffersAlert(product : ApphudProduct) {
+        let controller = UIAlertController(title: "You already have subscription for this product", message: "would you like to activate promo offer?", preferredStyle: .alert)
+        
+        for discount in product.skProduct?.discounts ?? [] {
+            controller.addAction(UIAlertAction(title: "Purchase Promo: \(discount.identifier!)", style: .default, handler: { act in
+                self.purchasePromo(product: product, promoID: discount.identifier!)
+            }))
+        }
+        
+        controller.addAction(UIAlertAction(title: "Purchase Product As Usual", style: .destructive, handler: { act in
+            self.purchaseProduct(product: product)
+        }))
+        
+        controller.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(controller, animated: true, completion: nil)
+    }
 }
 
-extension ViewController : ApphudDelegate {
-    
-    func apphudDidFetchStoreKitProducts(_ products: [SKProduct]) {
-        self.reload()
+// MARK: - TableView DataSource methods
+extension ViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        1 + paywalls.count
     }
     
-    func apphudSubscriptionsUpdated(_ subscriptions: [ApphudSubscription]) {
-        self.reload()
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        section == 0 ? rowsActions.count : paywalls[section - 1].products.count
     }
-}
-
-extension ViewController : ApphudUIDelegate {
     
-    func apphudScreenPresentationStyle(controller: UIViewController) -> UIModalPresentationStyle {
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            return .pageSheet
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "productCell", for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = rowsActions[indexPath.row].0
+            cell.detailTextLabel?.text = nil
         } else {
-            return .overFullScreen
+            let paywall = paywalls[indexPath.section - 1]
+            let product = paywall.products[indexPath.item]
+            
+            let text = product.skProduct?.getFullSubscriptionInfoString() ?? "PRODUCT UNAVAILABLE: \(product.productId)"
+            cell.textLabel?.text = text
+            if let subscription = Apphud.subscriptions()?.first(where: {$0.productId == product.productId}) {
+                cell.detailTextLabel?.text = subscription.expiresDate.description(with: Locale.current) + "\nState: \(subscription.status.toStringDuplicate())\nIntroductory used:\(subscription.isIntroductoryActivated)".uppercased()
+            } else {
+                cell.detailTextLabel?.text = "Not active"
+            }
+        }
+        
+        return cell
+    }
+}
+ 
+// MARK: - TableView Delegate methods
+extension ViewController {
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        section == 0 ? "ACTIONS" : "PRODUCTS"
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.section == 0 {
+            rowsActions[indexPath.row].1()
+            return
+        }
+        
+        let paywall = paywalls[indexPath.section - 1]
+        let product = paywall.products[indexPath.item]
+
+        if #available(iOS 12.2, *) {
+            if product.skProduct?.discounts.count ?? 0 > 0 {
+                // purchase promo offer
+                showPromoOffersAlert(product: product)
+            } else {
+                purchaseProduct(product: product)
+            }
+        } else {
+            purchaseProduct(product: product)
         }
     }
 }
