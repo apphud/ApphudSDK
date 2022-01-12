5e965648ce858103f20750cf0c330ad79baf8c91
Valery <linkedinValery@gmail.com>
2022-01-12 11:42:15 +0300

    up version -> 2.5.3

--- ApphudSDK/Apphud.swift
+++ ApphudSDK/Apphud.swift
@@ -13,7 +13,7 @@ import StoreKit
 import Foundation
 import UserNotifications
 
-internal let apphud_sdk_version = "2.5.2"
+internal let apphud_sdk_version = "2.5.3"
 
 public typealias ApphudEligibilityCallback = (([String: Bool]) -> Void)
 public typealias ApphudBoolCallback = ((Bool) -> Void)
