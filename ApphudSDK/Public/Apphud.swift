bea28b4dbf0988817871e6292102b4e0d84559bc
ren6 <ren6@apphud.com>
2023-11-16 14:34:51 +0700

    bump version

--- ApphudSDK/Public/Apphud.swift
+++ ApphudSDK/Public/Apphud.swift
@@ -14,7 +14,7 @@ import Foundation
 import UserNotifications
 import SwiftUI
 
-internal let apphud_sdk_version = "3.1.3"
+internal let apphud_sdk_version = "3.1.4"
 
 /**
  Public Callback object provide -> [String: Bool]
