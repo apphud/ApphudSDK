170c56ca60e834ff749b651b3649a6dbdd8b6183
Ren6 <ren6@apphud.com>
2026-07-27 15:32:33 +0300

    bump release 4.4.7

--- Sources/Public/Apphud.swift
+++ Sources/Public/Apphud.swift
@@ -14,7 +14,7 @@ import Foundation
 import UserNotifications
 import SwiftUI
 
-internal let apphud_sdk_version = "4.4.6"
+internal let apphud_sdk_version = "4.4.7"
 
 public enum ApphudDeeplinkAttributionKind {
     case direct
